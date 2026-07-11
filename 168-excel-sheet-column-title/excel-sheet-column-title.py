db21382176e2f04ca728c9b2c1d43e4a3651270c
Sagar Jamadar <sagarjamadar8989@gmail.com>
2026-07-11 23:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.23%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+        
+        ans = ""
+        
+        while columnNumber > 0:
+            columnNumber -= 1 
+            ans += chr(columnNumber%26 + ord("A"))
+            columnNumber //= 26
+
+        return ans[::-1]
+            
