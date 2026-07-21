5ebc45d6fcc1baf329e2115f02ba79d4507a5b33
Sagar Jamadar <sagarjamadar8989@gmail.com>
2026-07-21 23:37:08 +0530

    Time: 2560 ms (0.00%) | Memory: 25.4 MB (0.00%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def maxActiveSectionsAfterTrade(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+  
+        base_ones = s.count('1')
+
+        t = '1' + s + '1'
+
+        runs = []
+        i = 0
+        while i < len(t):
+            j = i
+            while j < len(t) and t[j] == t[i]:
+                j += 1
+            runs.append((t[i], j - i))
+            i = j
+
+        max_gain = 0
+
+        for i in range(1, len(runs) - 1):
+            if (
+                runs[i][0] == '1'
+                and runs[i - 1][0] == '0'
+                and runs[i + 1][0] == '0'
+            ):
+                gain = runs[i - 1][1] + runs[i + 1][1]
+                max_gain = max(max_gain, gain)
+
+        return base_ones + max_gain
