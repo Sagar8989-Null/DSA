892092615fd91d2b8b13fca99a93615281c7acaf
Sagar Jamadar <sagarjamadar8989@gmail.com>
2026-07-23 01:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (56.80%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -12,13 +12,19 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        if not root:
-            return []
+        result = []
+        stack = []
+        curr = root
 
-        return (
-            self.inorderTraversal(root.left)
-            + [root.val]
-            + self.inorderTraversal(root.right)
-        )
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+
+            curr = stack.pop()
+            result.append(curr.val)
+            curr = curr.right
+
+        return result
 
 
